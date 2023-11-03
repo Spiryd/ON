6ede1d8ae014175876dee4cdd311e74d79e8b808
Spiryd <neumann.maks@gmail.com>
2023-11-03 22:34:10 +0100

    fixed bug

--- l2/1.jl
+++ l2/1.jl
@@ -113,10 +113,10 @@ function d(x, y)
     return partial_pos + partial_neg
 end
 
-x1_1::Vector{Float64} = [2.718281828, -3.141592654, 1.414213562, 0.577215664, 0.301029995]
+x1_1::Vector{Float64} = [2.718281828, -3.141592654, 1.414213562, 0.5772156649, 0.3010299957]
 y1_1::Vector{Float64} = [1486.2497, 878366.9879, -22.37492, 4773714.647, 0.000185049]
 
-x1_2::Vector{Float32} = [2.718281828, -3.141592654, 1.414213562, 0.577215664, 0.301029995]
+x1_2::Vector{Float32} = [2.718281828, -3.141592654, 1.414213562, 0.5772156649, 0.3010299957]
 y1_2::Vector{Float32} = [1486.2497, 878366.9879, -22.37492, 4773714.647, 0.000185049]
 
 x2_1::Vector{Float64} = [2.718281828, -3.141592654, 1.414213562, 0.577215664, 0.301029995]
@@ -131,20 +131,32 @@ println(b(x1_1, y1_1))
 println(c(x1_1, y1_1))
 println(d(x1_1, y1_1))
 
-println("Original Float32: ")
-println(a(x1_2, y1_2))
-println(b(x1_2, y1_2))
-println(c(x1_2, y1_2))
-println(d(x1_2, y1_2))
-
 println("New Float64: ")
 println(a(x2_1, y2_1))
 println(b(x2_1, y2_1))
 println(c(x2_1, y2_1))
 println(d(x2_1, y2_1))
 
+println("Różnica Float64: ")
+println(abs(a(x1_1, y1_1) - a(x2_1, y2_1)))
+println(abs(b(x1_1, y1_1) - b(x2_1, y2_1)))
+println(abs(c(x1_1, y1_1) - c(x2_1, y2_1)))
+println(abs(d(x1_1, y1_1) - d(x2_1, y2_1)))
+
+println("Original Float32: ")
+println(a(x1_2, y1_2))
+println(b(x1_2, y1_2))
+println(c(x1_2, y1_2))
+println(d(x1_2, y1_2))
+
 println("New Float32: ")
 println(a(x2_2, y2_2))
 println(b(x2_2, y2_2))
 println(c(x2_2, y2_2))
 println(d(x2_2, y2_2))
+
+println("Różnica Float32: ")
+println(abs(a(x2_2, y2_2) - a(x1_2, y1_2)))
+println(abs(b(x2_2, y2_2) - b(x1_2, y1_2)))
+println(abs(c(x2_2, y2_2) - c(x1_2, y1_2)))
+println(abs(d(x2_2, y2_2) - d(x1_2, y1_2)))
